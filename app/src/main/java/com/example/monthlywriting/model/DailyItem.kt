4d1d552e44776a811e151cd11c66fbc814c39ee3
dongk00 <dddongk00@gmail.com>
2022-01-30 23:55:39 +0900

    TypeConverters

--- app/src/main/java/com/example/monthlywriting/model/DailyItem.kt
+++ app/src/main/java/com/example/monthlywriting/model/DailyItem.kt
@@ -2,6 +2,7 @@ package com.example.monthlywriting.model
 
 import android.graphics.Bitmap
 import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -12,10 +13,13 @@ data class DailyWritingItem(
     @ColumnInfo (name = "month") var month : String,
     @ColumnInfo (name = "type") var type : String,
     @ColumnInfo (name = "name") var name : String,
-    @ColumnInfo (name = "dailymemo") var dailyMemo : Map<String, DailyMemo>
+    @ColumnInfo (name = "days") var days : String?,
+    @ColumnInfo (name = "times") var times : String?,
+    @Embedded var dailymemo : DailyMemo
 )
 
 data class DailyMemo(
+    var date : String,
     var done : Boolean,
     var memo : String,
     var photo : Bitmap
--- app/src/main/java/com/example/monthlywriting/room/AppDataBase.kt
+++ app/src/main/java/com/example/monthlywriting/room/AppDataBase.kt
@@ -2,12 +2,14 @@ package com.example.monthlywriting.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.monthlywriting.model.DailyWritingItem
 
 @Database(
     entities = [DailyWritingItem::class],
     version = 1,
     exportSchema = false)
+@TypeConverters(RoomConverters::class)
 abstract class AppDataBase  : RoomDatabase() {
     abstract fun dailyWritingItemDao() : DailyWritingItemDao
 }
